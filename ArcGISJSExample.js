9af23268cfb20b390e87353630c60786d1800b20
Andrew Wang <zxwandrew@gmail.com>
2015-07-25 17:46:22 -0700

    first commit

--- ArcGISJSExample.js
+++ ArcGISJSExample.js
@@ -1,23 +1,65 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  var routePath = "https://js.arcgis.com/4.0beta1",
+    routeLoaded = false,
+    loadHandler = function() {
+      routeLoaded = true;
+    };
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Router.route('/', {
+    verbose: true,
+    name: 'home',
+    template: 'ArcGIS',
+    controller: PreloadController,
+    preload: {
+      timeOut: 5000,
+      styles: ['https://js.arcgis.com/4.0beta1/esri/css/esri.css', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'],
+      sync: routePath,
+      onBeforeSync: function(fileName) {
+        if (fileName === routePath) {
+          var script = document.createElement('script');
+          script.rel = 'preload javascript';
+          script.type = 'text/javascript';
+          script.src = routePath;
+          script.onload = loadHandler;
+          document.body.appendChild(script);
+          return false;
+        }
+      },
+      onSync: function(fileName) {
+        if (routeLoaded && fileName === routePath) {
+          return !!require && !!define;
+        }
+      },
+      onAfterSync: function(fileName) {
+        return false;
+      }
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.ArcGIS.rendered = function() {
+    var map, view;
+
+    if (routeLoaded) {
+      require([
+        "esri/Map",
+        "esri/views/SceneView",
+        "dojo/domReady!"
+      ], function(Map, SceneView) {
+        map = new Map({
+          basemap: "streets"
+        });
+
+        view = new SceneView({
+          container: "viewDiv",
+          map: map,
+          scale: 240000000
+        })
+
+      })
     }
-  });
+  };
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+  Meteor.startup(function() {});
 }
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # ArcGISJsExample
-An Example to use the ArcGIS Javascript API with MeteorJS
+An Example to use the ArcGIS Javascript API 4.0 with MeteorJS
+
+## Introduction ##
+
+This is a very simple application created with the ArcGIS JavaScript API 4.0 (Beta 1) for the MeteorJS framework.
+
+This projects has the following dependencies:
+
+* iron:router
+* miro:preloader
+
+optional:
+* jquery
+* Bootstrap
+
+And of course ArcGIS Javascript API.
+
+Because of the require structure structure used by the ArcGIS library, you will need to use the miro:preloader.
+Since Javascript Samples are also written with the help of JQuery and Bootstrap, I have also included the 2 libraries.
